ae70a84e93b53b54a3875e2b9d1dcfcb82a4795e
h-ARTS <hi5hanan@yahoo.com>
2017-10-07 20:31:05 +0200

    Edit LeaderService and Templates

--- src/app/services/leader.service.ts
+++ src/app/services/leader.service.ts
@@ -12,6 +12,10 @@ export class LeaderService {
     return LEADERS;
   }
 
+  getLeader(id: number): Leader {
+    return LEADERS.filter((leader) => (leader.id === id))[0];
+  }
+
   getFeaturedLeader(): Leader {
     return LEADERS.filter((leader) => (leader.featured))[0];
   }
