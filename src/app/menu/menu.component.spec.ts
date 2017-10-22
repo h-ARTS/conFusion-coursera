53e887e54a4c00162f94a0115f9f7cee75daffcd
h-ARTS <hi5hanan@yahoo.com>
2017-10-22 23:22:03 +0200

    Components Test

--- src/app/menu/menu.component.spec.ts
+++ src/app/menu/menu.component.spec.ts
@@ -1,16 +1,52 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { MenuComponent } from './menu.component';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { Dish } from '../shared/dish';
+import { DishService } from '../services/dish.service';
+import { DISHES } from '../shared/dishes';
+import { baseURL } from '../shared/baseurl';
+import { Observable } from 'rxjs/Observable';
 
 describe('MenuComponent', () => {
   let component: MenuComponent;
   let fixture: ComponentFixture<MenuComponent>;
 
+  // tslint:disable-next-line:prefer-const
+  let dishServiceStub = {
+    getDishes: function(): Observable<Dish[]> {
+      return Observable.of(DISHES);
+    }
+  }
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MenuComponent ]
+      imports: [
+        BrowserAnimationsModule,
+        MaterialModule,
+        FlexLayoutModule,
+        RouterTestingModule.withRoutes([
+          {
+            path: 'menu',
+            component: MenuComponent
+          }
+        ])
+      ],
+      declarations: [ MenuComponent ],
+      providers: [
+        { provide: DishService, useValue: dishServiceStub },
+        { provide: 'BaseURL', useValue: baseURL}
+      ]
     })
     .compileComponents();
+
+    // tslint:disable-next-line:prefer-const
+    let dishService = TestBed.get(DishService);
   }));
 
   beforeEach(() => {
@@ -22,4 +58,23 @@ describe('MenuComponent', () => {
   it('should be created', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should contain 4 dish items', () => {
+    expect(component.dishes.length).toBe(4);
+    expect(component.dishes[1].name).toBe('Zucchipakoda');
+    expect(component.dishes[3].featured).toBeFalsy();
+  });
+
+  it('should use dishes template', () => {
+    fixture.detectChanges();
+
+    let de: DebugElement;
+    let el: HTMLElement;
+
+    de = fixture.debugElement.query(By.css('h1'));
+    el = de.nativeElement;
+
+    expect(el.textContent).toContain(DISHES[0].name.toUpperCase());
+  });
+
 });
